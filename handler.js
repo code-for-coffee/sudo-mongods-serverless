b1677fe7c1e3c1a190a63fbe1d1c8d4b7c903897
code-for-coffee <jtraverjr@gmail.com>
2017-06-23 20:43:12 -0500

    adding flavour endpoint

--- handler.js
+++ handler.js
@@ -6,6 +6,7 @@ AWS.config.update({ region: process.env.REGION });
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 const categoriesTable = process.env.CATEGORIES_TABLE;
+const flavoursTable = process.env.FLAVOURS_TABLE;
 
 export async function createCategory (event, context, callback) {
   const requestBody = JSON.parse(event.body);
@@ -93,6 +94,110 @@ export async function updateCategory (event, context, callback) {
 }
 
 export async function deleteCategory (event, context, callback) {
+  const params = {
+    TableName: categoriesTable,
+    Key: {
+      id: event.pathParameters.id
+    }
+  };
+
+  dynamoDb.delete(params, (err, result) => {
+    if (err) {
+      callback(null, failure({err, result}));
+      return;
+    }
+
+    // Successful
+    callback(null, success());
+  });
+}
+
+export async function createFlavour (event, context, callback) {
+  const requestBody = JSON.parse(event.body);
+
+  const params = {
+    TableName: categoriesTable,
+    Item: {
+      id: uuid.v1(),
+      scent: requestBody.scent,
+      createdAt: new Date().getTime(),
+    },
+  };
+
+  dynamoDb.put(params, (err, data) => {
+    if (err) {
+      callback(null, failure({err, data}));
+      return;
+    }
+
+    // Successful
+    callback(null, success(params.Item));
+  });
+}
+
+export async function listFlavours (event, context, callback) {
+  const params = {
+    TableName: categoriesTable
+  };
+
+  dynamoDb.scan(params, (err, data) => {
+    if (err) {
+      callback(null, failure({err, data}));
+      return;
+    }
+
+    // Successful
+    callback(null, success(data));
+  });
+}
+
+export async function getFlavour (event, context, callback) {
+  const params = {
+    TableName: categoriesTable,
+    Key: {
+      id: event.pathParameters.id
+    }
+  };
+
+  dynamoDb.get(params, (err, result) => {
+    if (err) {
+      callback(null, failure({err, result}));
+      return;
+    }
+
+    // Successful
+    callback(null, success(result.Item));
+  });
+}
+
+export async function updateFlavour (event, context, callback) {
+  const requestBody = JSON.parse(event.body);
+
+  const params = {
+    TableName: categoriesTable,
+    Key: {
+      id: event.pathParameters.id,
+    },
+    ExpressionAttributeValues: {
+      ':scent': requestBody.scent,
+      ':updatedAt': new Date().getTime(),
+    },
+    UpdateExpression: 'SET scent = :scent, updatedAt = :updatedAt',
+    ReturnVAlues: 'ALL_NEW'
+  };
+
+  dynamoDb.update(params, (err, result) => {
+    if (err) {
+      callback(null, failure({err, result}));
+      return;
+    }
+
+    // Successful
+    callback(null, success(result.Attributes));
+  });
+}
+
+export async function deleteFlavour (event, context, callback) {
   const params = {
     TableName: categoriesTable,
     Key: {
